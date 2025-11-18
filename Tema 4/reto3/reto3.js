3938b56fd626fdeb92b2452d494d2c286e0db8d3
Elenagm-ewewe <elenabodom@gmail.com>
2025-11-18 19:26:59 +0100

    empiezo rto3

---
+++ Tema 4/reto3/reto3.js
@@ -0,0 +1,8 @@
+ let letras = ["A" , "B", "C" , "D", "E", "F", "G", "H", "I","J", "K","L","M","N","O","P","Q","R","S","T", "U", "V", "W", "X","Y","Z"];
+
+
+
+ let numerocuenta = document.getElementById("cuenta").value;
+
+for(let i = 0 ; i < letras.length ; i++){
+ }
