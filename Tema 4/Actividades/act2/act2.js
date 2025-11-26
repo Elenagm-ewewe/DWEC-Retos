a8fecff74adef981b600615b14c3465cb472f776
Elenagm-ewewe <egutmor699@g.educaand.es>
2025-11-26 02:30:32 +0100

    reto 1

--- Tema 4/Actividades/act2/act2.js
+++ Tema 4/Actividades/act2/act2.js
@@ -1,35 +1,30 @@
-let coloractual = "255, 255, 255"
+let coloractual = "255, 255, 255";
 
-document.getElementById("botonaso").addEventListener("click", function(e){
-e.preventDefault();
+document.addEventListener("dblclick", function (e) {
+  e.preventDefault();
 
+  let contenedor = document.body;
+  let botonguardar = document.getElementById("guardar");
 
-let contenedor = document.getElementById("color");
-let botonguardar = document.getElementById("guardar");
+  let color = generarColor();
+  contenedor.style.backgroundColor = `rgb(${color})`;
+  botonguardar.style.backgroundColor = `rgb(${color})`;
 
-let color = generarColor();
-contenedor.style.backgroundColor = `rgb(${color})`
-botonguardar.style.backgroundColor = `rgb(${color})`
-
-coloractual = color;
+  coloractual = color;
 });
 
+document.getElementById("guardar").addEventListener("click", function (event) {
+  event.preventDefault();
 
-document.getElementById("guardar").addEventListener("click", function(event){
-event.preventDefault();
+  let elemento = document.getElementById("guardados");
 
-let elemento = document.getElementById("guardados");
-
-elemento.innerHTML += `<strong style="color: rgb(${coloractual});" >RGB(${coloractual})</strong><div style="color: rgb(${coloractual}); width:20px; height:20px"></div>`
-
-
+  elemento.innerHTML += `<div class="guardao"><strong style="color: rgb(${coloractual});" >RGB(${coloractual})</strong></div>`;
 });
 
-function generarColor(){
- 
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    
-    return r.toString() + "," + g.toString() + "," + b.toString()
+function generarColor() {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+
+  return r.toString() + "," + g.toString() + "," + b.toString();
 }
---
+++ Tema 5/Retos/reto1.js
@@ -0,0 +1,29 @@
+let paises = document.getElementById("paises");
+let listaNombres = [];
+obtenerNombres();
+
+document.getElementById("busqueda").addEventListener("input", function(e){
+
+let buscar = document.getElementById("busqueda").value.toLowerCase();
+
+let encontrados = listaNombres.filter(p => p.name.common.toLowerCase().includes(buscar) || p.name.common.toLowerCase().includes(buscar) );
+
+let html = `<tr><th>Nombre</th><th>Capital</th><th>Region</th><th>Poblacion</th>`
+html += encontrados.map(e => `</tr><tr><td>${e.name.common}</td> <td>${e.capital}</td> <td>${e.region}</td> <td>${e.population}</td></tr>`).join("");
+
+paises.innerHTML = html;
+
+});
+
+//--------------------------------------------------------------------------
+
+    async function obtenerNombres() {
+        try {
+            const respuesta = await fetch("https://restcountries.com/v3.1/all?fields=capital,name,region,population");
+            listaNombres = await respuesta.json();
+            
+        } catch (error) {
+            console.error("Error:", error);
+        }
+        
+    }
